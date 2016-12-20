ebea7475184d18068356512de4c935754d8a8620
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-12-20 12:17:20 +0000

    Intents and Layout changes
    Working

--- src/androidStudio/Catchy/app/src/main/java/android/app/navjot/catchy/MainActivity.java
+++ src/androidStudio/Catchy/app/src/main/java/android/app/navjot/catchy/MainActivity.java
@@ -6,20 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    //declaring buttons
-    Button callButton;
-    Button locationButton;
-    Button grosseryButton;
-    Button cameraButton;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //declaring buttons
+        Button callButton;
+        Button locationButton;
+        Button grosseryButton;
+        Button cameraButton;
 
         //setting connection using find view by id which helps us connect code with layout elements by their id's
         callButton = (Button) findViewById(R.id.callButton);
@@ -30,12 +31,13 @@ public class MainActivity extends AppCompatActivity {
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Call Button Pressed", Toast.LENGTH_SHORT).show();
                 //This Explicit Intent is done with the help of this tutorial
                 //http://www.javatpoint.com/android-explicit-intent-example
-                Intent openTelephonyActivityIntent = new Intent(getApplicationContext(), TelephonyActivity.class);
-                openTelephonyActivityIntent.putExtra("welcomeMessage","Welcome to Telephony Activity");
-                openTelephonyActivityIntent.putExtra("callToActionMessage", "You can make a call from here");
-
+                Intent startTelephonyActivityIntent = new Intent(getApplicationContext(), TelephonyActivity.class);
+                startTelephonyActivityIntent.putExtra("welcomeMessage","Welcome to Telephony Activity");
+                startTelephonyActivityIntent.putExtra("callToActionMessage", "You can make a call from here");
+                startActivity(startTelephonyActivityIntent);
             }
         });
     }
--- src/androidStudio/Catchy/app/src/main/java/android/app/navjot/catchy/TelephonyActivity.java
+++ src/androidStudio/Catchy/app/src/main/java/android/app/navjot/catchy/TelephonyActivity.java
@@ -2,12 +2,27 @@ package android.app.navjot.catchy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TelephonyActivity extends AppCompatActivity {
+
+    EditText enterNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telephony);
+
+        //This Explicit Intent is done with the help of this tutorial
+        //http://www.javatpoint.com/android-explicit-intent-example
+        Bundle extras = getIntent().getExtras();
+        String welcomeMessage = extras.getString("welcomeMessage");
+        String callToActionMessage = extras.getString("callToActionMessage");
+        Toast.makeText(getApplicationContext(), welcomeMessage + "\n" + callToActionMessage  ,Toast.LENGTH_LONG).show();
+        //This is the result part of the intent
+
+        enterNumber = (EditText) findViewById(R.id.enterNumber);
+
     }
 }
